b2564a80380c99911d666a59687d54f9a65f3024
nq.maigre <nq.maigre@gmail.com>
2018-02-14 23:18:49 +0800

    添加通过菜单打开视频文件的功能

--- script/elements.js
+++ script/elements.js
@@ -1,5 +1,6 @@
 const splayer = document.getElementById('splayer');
 const widget = document.getElementById('widget');
+const video = document.getElementById('video');
 
 const displayWidget = function() {
     widget.style.display = 'block';
@@ -12,7 +13,7 @@ const hideWidget = function() {
 splayer.addEventListener('mouseover', displayWidget, false);
 splayer.addEventListener('mouseout', hideWidget, false);
 
-module.exports.splayer = splayer;
+//module.exports.splayer = splayer;
 module.exports = {
-    splayer: splayer
+    video: video
 }
--- script/window-action.js
+++ script/window-action.js
@@ -1,10 +1,28 @@
-const {ipcRenderer} = require('electron');
+const {ipcRenderer, remote} = require('electron');
+const {video} = require('./elements.js');
+
+function getVideoFile() {
+	const files = remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+		filters: [
+			{name: 'Video Files', extensions: ['mp4', 'mkv', 'mov']}
+		],
+		properties: ['openFile']
+	});
+
+	if(files) {
+		return files[0];
+	} else {
+		return null;
+	}
+}
 
 function processMessage(event, tag) {
     switch(tag){
         case('open'):
-            //do something
-            alert('This is a test.');
+            const video_file = getVideoFile();
+            if(video_file) {
+            	video.setAttribute('src', video_file);
+            }
             break;
     }
 }
