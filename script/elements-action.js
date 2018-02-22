efe66551d681fe74194d69f0ffe5b79fd309d6a6
lilacvapor <lilacvapor@gmail.com>
2018-02-22 15:32:15 +0800

    功能更新: 时间进度显示

--- script/elements-action.js
+++ script/elements-action.js
@@ -1,21 +1,78 @@
-const {widget, splayer, video} = require('./elements.js');
+const elements = require('./elements.js');
 
 const displayWidget = function() {
-    widget.className = 'widget-mouseover';
+    elements.widget.className = 'widget-mouseover';
 }
 
 const hideWidget = function() {
-    widget.className = 'widget';
+    elements.widget.className = 'widget';
 }
 
 const togglePlayPause = function() {
-    if (video.paused) {
-        video.play();
+    if (elements.video.paused) {
+        elements.video.play();
     } else {
-        video.pause();
+        elements.video.pause();
     }
 }
 
-splayer.addEventListener('mouseover', displayWidget, false);
-splayer.addEventListener('mouseout', hideWidget, false);
-splayer.addEventListener('click', togglePlayPause, false);
+/**
+ * 时间秒数格式化
+ * @param s 时间戳（单位：秒）
+ * @returns {*} 格式化后的时分秒
+ */
+const formatTime = function(time) {
+    var formatted_time;
+    if(time > -1) {
+        var hour = parseInt(time/3600);
+        var minute = parseInt(time/60);
+        var second = parseInt(time%60);
+
+        if(hour < 10) {
+            formatted_time = '0'+ hour + ':';
+        } else {
+            formatted_time = hour + ':';
+        }
+        if(minute < 10) { 
+        	formatted_time += '0' + minute + ':';
+        } else {
+        	formatted_time += minute + ":";
+        }
+        if(second < 10) { 
+        	formatted_time += '0' + second;
+        } else {
+        	formatted_time += second;
+        }
+    }
+    console.log(formatted_time);
+    return formatted_time;
+}
+
+const updateCurrentTime = function() {
+	const time = formatTime(elements.video.currentTime);
+	const node = document.createTextNode(time);
+	const childs = elements.info_current_time.childNodes;
+	for(var i = childs.length - 1; i >= 0; i--) {    
+		elements.info_current_time.removeChild(childs.item(i));    
+	} 
+    elements.info_current_time.appendChild(node);
+}
+
+elements.video.onloadedmetadata = function() {
+	const time = formatTime(elements.video.duration);
+	const node = document.createTextNode(time);
+	const childs = elements.info_current_time.childNodes;
+	for(var i = childs.length - 1; i >= 0; i--) {    
+		elements.info_duration.removeChild(childs.item(i));    
+	} 
+    elements.info_duration.appendChild(node);
+}
+
+//为 <video> 元素添加 ontimeupdate 事件，如果当前播放位置改变则执行函数 
+elements.video.ontimeupdate = function() { 
+	updateCurrentTime();
+};
+
+elements.splayer.addEventListener('mouseover', displayWidget, false);
+elements.splayer.addEventListener('mouseout', hideWidget, false);
+elements.splayer.addEventListener('click', togglePlayPause, false);
--- script/elements.js
+++ script/elements.js
@@ -2,11 +2,15 @@ const splayer = document.getElementById('splayer');
 const widget = document.getElementById('widget');
 const video = document.getElementById('video');
 const button_play = document.getElementById('button-play');
+const info_current_time = document.getElementById('info-current-time');
+const info_duration = document.getElementById('info-duration');
 
 //module.exports.splayer = splayer;
 module.exports = {
     splayer: splayer,
     widget: widget,
     video: video,
-    button_play: button_play
+    button_play: button_play,
+    info_current_time: info_current_time,
+    info_duration: info_duration
 }
